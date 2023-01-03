678e0974936bb88f831c194b241193195f6bb5e7
Roman <Alimov.ramon@gmail.com>
2023-01-03 15:12:28 +0300

    eleventh video

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+
 
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
+        <div className="app">
+            <header className={'header'}>
+                <img className={'logo'} src={'https://sun9-82.userapi.com/s/v1/ig2/LLdk_N-fVpPSiwyNpF7h-B585nSmRYtB1ZNiNFAFfx6o0bUNC94OjOlbRChB9XxaKUIqEgEvVVEwUyFvrX6gSH0s.jpg?size=200x200&quality=95&crop=0,0,729,729&ava=1'} alt={'logo'} />
+                Header
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a href={'#'}>Profile</a>
+                </div>
+                <div>
+                    <a href={'#'}>Message</a>
+                </div>
+                <div>
+                    <a href={'#'}>News</a>
+                </div>
+                <div>
+                    <a href={'#'}>Music</a>
+                </div>
+                <div>
+                    <a href={'#'}>Settings</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img className={'banner'} src={'https://kipmu.ru/wp-content/uploads/rost.jpg'} alt={'banner'} />
+                </div>
+                <div>
+                    avatar+description
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    New Posts
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
         </div>
     );
 }
