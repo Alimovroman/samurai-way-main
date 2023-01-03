82c30e37e6609a2c50c189a957f70ff218aab4a8
Roman <Alimov.ramon@gmail.com>
2023-01-03 15:42:02 +0300

    twelfth video

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Content from "./components/Content/Content";
 
 
 
 function App() {
     return (
         <div className="app">
-            <header className={'header'}>
-                <img className={'logo'} src={'https://sun9-82.userapi.com/s/v1/ig2/LLdk_N-fVpPSiwyNpF7h-B585nSmRYtB1ZNiNFAFfx6o0bUNC94OjOlbRChB9XxaKUIqEgEvVVEwUyFvrX6gSH0s.jpg?size=200x200&quality=95&crop=0,0,729,729&ava=1'} alt={'logo'} />
-                Header
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a href={'#'}>Profile</a>
-                </div>
-                <div>
-                    <a href={'#'}>Message</a>
-                </div>
-                <div>
-                    <a href={'#'}>News</a>
-                </div>
-                <div>
-                    <a href={'#'}>Music</a>
-                </div>
-                <div>
-                    <a href={'#'}>Settings</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-                <div>
-                    <img className={'banner'} src={'https://kipmu.ru/wp-content/uploads/rost.jpg'} alt={'banner'} />
-                </div>
-                <div>
-                    avatar+description
-                </div>
-                <div>
-                    My posts
-                </div>
-                <div>
-                    New Posts
-                </div>
-                <div>
-                    Post 1
-                </div>
-                <div>
-                    Post 2
-                </div>
-            </div>
+            <Header />
+            <NavBar />
+            <Content />
         </div>
     );
 }
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img className={'banner'} src={'https://kipmu.ru/wp-content/uploads/rost.jpg'} alt={'banner'} />
+            </div>
+            <div>
+                avatar+description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New Posts
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    );
+};
+
+export default Content;
